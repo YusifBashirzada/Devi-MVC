dc932f0813ba852850e9f21a6f4287ba47310829
YusifBashirzada <yusifsb-apa103@code.edu.az>
2026-05-25 01:27:00 +0400

    create portfolio detail page

--- Devi/Controllers/PortfolioController.cs
+++ Devi/Controllers/PortfolioController.cs
@@ -1,12 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Devi.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Devi.Controllers
 {
     public class PortfolioController : Controller
     {
+        private readonly AppDbContext _context;
+
+        public PortfolioController(AppDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+        
+        public async Task<IActionResult> Detail(int? id)
+        {
+            if (id == null || id < 1) return BadRequest();
+
+            var portfolio = await _context.Portfolios
+                .FirstOrDefaultAsync(p => p.Id == id);
+
+            if (portfolio == null) return NotFound();   
+
+            return View(portfolio);
         }
     }
 }
